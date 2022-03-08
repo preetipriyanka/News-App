c733c05017ad61069c317f5eff905dd8125d08d2
preetipriyanka24 <preetipriyanka24@gmail.com>
2022-03-08 08:01:26 +0530

    final commit

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,74 +1,58 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import linkedin from '../../images/linkedin.svg';
-import github from '../../images/github.svg';
+import React from "react";
+import { Link } from "react-router-dom";
+import linkedin from "../../images/linkedin.svg";
+import github from "../../images/github.svg";
 // import facebook from '../../images/facebook.svg';
 // import website from '../../images/website.png';
 
 function Footer() {
   return (
-    <footer className='footer'>
-      {/* <div className='footer__container'> */}
-        <p className='footer__copyright'>
-          © {new Date().getFullYear()} Priyanka Kumari | News API
-        </p>
-        <div className='footer__content-container'>
-          <ul className='footer__content footer__links'>
-            <li className='footer__item'>
-              {/* <a className='footer__link' href='/'>
+    <footer className="footer">
+      <p className="footer__copyright">
+        © {new Date().getFullYear()} Priyanka Kumari | News API
+      </p>
+      <div className="footer__content-container">
+        <ul className="footer__content footer__links">
+          <li className="footer__item">
+            {/* <a className='footer__link' href='/'>
               Home
             </a> */}
-              <Link className='footer__link' to='/'>
-                Home
-              </Link>
-            </li>
-            <li className='footer__item'>
-              <a
-                className='footer__link'
-                href='https://practicum.yandex.com/'
-                target='_blank'
-                rel='noreferrer'
-              >
-                Assignment By Conde Nest
-              </a>
-            </li>
-          </ul>
-          <ul className='footer__content footer__icons'>
-            <li>
-              <a
-                href='https://github.com/preetipriyanka'
-                target='_blank'
-                rel='noreferrer'
-              >
-                <img src={github} alt='github' className='footer__icon' />
-              </a>
-            </li>
-            {/* <li>
+            <Link className="footer__link" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="footer__item">
             <a
-              href='https://linda-kovacs.com/'
-              target='_blank'
-              rel='noreferrer'
+              className="footer__link"
+              href="https://www.condenast.com/"
+              target="_blank"
+              rel="noreferrer"
             >
-              <img src={website} alt='website' className='footer__icon' />
+              Assignment By Conde Nast
             </a>
-          </li> */}
-            <li>
-              <a
-                href='https://www.linkedin.com/in/preetipriyanka24/'
-                target='_blank'
-                rel='noreferrer'
-              >
-                <img src={linkedin} alt='facebook' className='footer__icon' />
-              </a>
-            </li>
-            {/* <li>
-            <a href='https://www.facebook.com' target='_blank' rel='noreferrer'>
-              <img src={facebook} alt='facebook' className='footer__icon ' />
+          </li>
+        </ul>
+        <ul className="footer__content footer__icons">
+          <li>
+            <a
+              href="https://github.com/preetipriyanka"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={github} alt="github" className="footer__icon" />
             </a>
-          </li> */}
-          </ul>
-        </div>
-      {/* </div> */}
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/preetipriyanka24/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={linkedin} alt="facebook" className="footer__icon" />
+            </a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 }
